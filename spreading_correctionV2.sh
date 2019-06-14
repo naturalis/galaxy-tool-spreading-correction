c6fc85db9cc434e1e4746c8a7d5deb3d52325b74
MartenHoogeveen <martenhoogeveen@gmail.com>
2019-06-14 14:08:58 +0000

    New galaxy update

--- spreading_correctionV2.sh
+++ spreading_correctionV2.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-#location for production server
+outlocation=$(mktemp -d /home/galaxy/galaxy/database/files/XXXXXX)
 #outlocation=$(mktemp -d /media/GalaxyData/database/files/XXXXXX)
-#location for the testserver
-#outlocation=$(mktemp -d /media/GalaxyData/files/XXXXXX)
-outlocation=$(mktemp -d /home/galaxy/ExtraRef/XXXXXX)
-create_input.py -t $1 -n $2 -ot $outlocation"/transposed.txt" -o $outlocation"/nextera_transposed.txt"
+
+SCRIPTDIR=$(dirname "$(readlink -f "$0")")
+
+python $SCRIPTDIR"/create_input.py" -t $1 -n $2 -ot $outlocation"/transposed.txt" -o $outlocation"/nextera_transposed.txt"
 sed 's/\t/ /g' $outlocation"/nextera_transposed.txt" > $outlocation"/nextera_transposed_space.txt"
-#echo "${6}"
 if [ "${6}" == "True" ]
 then
-  unspread_manual.py $outlocation"/nextera_transposed_space.txt" --i5 'S.index.name' --i7 'N.index.name' --column 0 --sep ' ' --rows $7 --cols $8 --conditional_spreading_input "${9}" --c "${10}" --output_folder $outlocation"/" > $outlocation"/log.txt"
+  python $SCRIPTDIR"/unspread_manual.py" $outlocation"/nextera_transposed_space.txt" --i5 'S.index.name' --i7 'N.index.name' --column 0 --sep ' ' --rows $7 --cols $8 --conditional_spreading_input "${9}" --c "${10}" --output_folder $outlocation"/" > $outlocation"/log.txt"
   cat $outlocation"/log.txt" >> $outlocation"/log2.txt"
 else
-  unspread.py $outlocation"/nextera_transposed_space.txt" --i5 'S.index.name' --i7 'N.index.name' --column 0 --sep ' ' --rows $7 --cols $8 --h $9 --c "${10}" --t "${11}" --output_folder $outlocation"/" > $outlocation"/log.txt"
+  python $SCRIPTDIR"/unspread.py" $outlocation"/nextera_transposed_space.txt" --i5 'S.index.name' --i7 'N.index.name' --column 0 --sep ' ' --rows $7 --cols $8 --h $9 --c "${10}" --t "${11}" --output_folder $outlocation"/" > $outlocation"/log.txt"
   cat $outlocation"/log.txt" $outlocation"/"*".log" >> $outlocation"/log2.txt"
 fi
 
